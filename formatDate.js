15f02cfaa965b105b49f63f9fd9d5dcf479912b2
Ezequiel <ezequielamin@outlook.com>
2022-05-11 16:26:43 -0300

    changed response

--- formatDate.js
+++ formatDate.js
@@ -5,12 +5,12 @@ const formatDate = (fecha) => {
     fecha.split("-")[2]
   );
 
-  let day = new Intl.DateTimeFormat("es-AR", { weekday: "short" }).format(date);
+  let day = new Intl.DateTimeFormat("es-AR", { weekday: "long" }).format(date);
   day = day.charAt(0).toUpperCase() + day.slice(1);
   let month = new Intl.DateTimeFormat("es-AR", { month: "long" }).format(date);
   month = month.charAt(0).toUpperCase() + month.slice(1);
 
-  return `${day} ${fecha.split("-")[2]} ${month} (${fecha.split("-")[2]}/${
+  return `${day} ${fecha.split("-")[2]} de ${month} (${fecha.split("-")[2]}/${
     fecha.split("-")[1]
   }/${fecha.split("-")[0]})`;
 };
