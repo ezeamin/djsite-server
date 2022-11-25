b1c910e197576b080f7b38b525a78a94fed7a91b
Ezequiel Amin <ezequielamin@outlook.com>
2022-11-25 07:13:25 -0300

    try catch for temporary error with google auth

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
-const app = express();
-require("./db/database");
-const routes = require("./routes/routes");
-const adminRoutes = require("./routes/adminRoutes");
+try {
+  const app = express();
+  require('./db/database');
+  const routes = require('./routes/routes');
+  const adminRoutes = require('./routes/adminRoutes');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://djezeamin.com",
-      "https://www.djezeamin.com",
-      "https://admin.djezeamin.com",
-      "https://djezeamin.vercel.app",
-    ],
-  })
-);
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(morgan('dev'));
+  app.use(
+    cors({
+      origin: [
+        'http://localhost:3000',
+        'https://djezeamin.com',
+        'https://www.djezeamin.com',
+        'https://admin.djezeamin.com',
+        'https://djezeamin.vercel.app',
+      ],
+    })
+  );
 
-app.use(routes);
-app.use(adminRoutes);
+  app.use(routes);
+  app.use(adminRoutes);
 
-app.set("trust proxy", true);
-app.set("port", process.env.PORT || 5000);
+  app.set('trust proxy', true);
+  app.set('port', process.env.PORT || 5000);
 
-app.listen(app.get("port"), (req, res) =>
-  console.log("Server running on port " + app.get("port"))
-);
+  app.listen(app.get('port'), (req, res) =>
+    console.log('Server running on port ' + app.get('port'))
+  );
+} catch (error) {
+  console.log(error);
+}
